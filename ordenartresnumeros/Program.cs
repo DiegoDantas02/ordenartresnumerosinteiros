2375a70961ec93e0978eba6a05f9137d0d0ac8e4
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-13 20:59:38 -0300

    Add project files.

---
+++ ordenartresnumeros/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OrdenarTresNumerosInteiros
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<int> _listDeInteiros  = new List<int>();
+            Console.WriteLine(" Digite Três Números Inteiros: ");
+        }
+    }
+}
